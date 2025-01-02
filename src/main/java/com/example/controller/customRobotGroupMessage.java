1170a0331a056643d7fa8a52a3df5047a8f626ef
胡长城 <hcc_sf@jztey.com>
2025-01-02 09:11:26 +0800

    dingding send demo

---
+++ src/main/java/com/example/controller/customRobotGroupMessage.java
@@ -0,0 +1,56 @@
+package com.example.controller;
+
+import com.dingtalk.api.DefaultDingTalkClient;
+import com.dingtalk.api.DingTalkClient;
+import com.dingtalk.api.request.OapiRobotSendRequest;
+import com.dingtalk.api.response.OapiRobotSendResponse;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.net.URLEncoder;
+import java.util.Arrays;
+
+public class customRobotGroupMessage {
+
+    public static final String CUSTOM_ROBOT_TOKEN = "xxxxx";
+
+    public static final String USER_ID= "<you need @ group user's userId>";
+
+    public static final String SECRET = "xxxxx";
+
+    public static void main(String[] args) {
+        try {
+            Long timestamp = System.currentTimeMillis();
+            System.out.println(timestamp);
+            String secret = SECRET;
+            String stringToSign = timestamp + "\n" + secret;
+            Mac mac = Mac.getInstance("HmacSHA256");
+            mac.init(new SecretKeySpec(secret.getBytes("UTF-8"), "HmacSHA256"));
+            byte[] signData = mac.doFinal(stringToSign.getBytes("UTF-8"));
+            String sign = URLEncoder.encode(Arrays.toString((Base64.encodeBase64(signData))),"UTF-8");
+            System.out.println(sign);
+            //sign字段和timestamp字段必须拼接到请求URL上，否则会出现 310000 的错误信息
+            DingTalkClient client = new DefaultDingTalkClient("https://oapi.dingtalk.com/robot/send?sign="+sign+"&timestamp="+timestamp);
+            OapiRobotSendRequest req = new OapiRobotSendRequest();
+            /**
+             * 发送文本消息
+             */
+            //定义文本内容
+            OapiRobotSendRequest.Text text = new OapiRobotSendRequest.Text();
+            text.setContent("xx，让进步发生");
+            //定义 @ 对象
+            OapiRobotSendRequest.At at = new OapiRobotSendRequest.At();
+            at.setAtUserIds(Arrays.asList(USER_ID));
+            //设置消息类型
+            req.setMsgtype("text");
+            req.setText(text);
+            req.setAt(at);
+            OapiRobotSendResponse rsp = client.execute(req, CUSTOM_ROBOT_TOKEN);
+            System.out.println(rsp.getBody());
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("发送失败");
+        }
+    }
+}
